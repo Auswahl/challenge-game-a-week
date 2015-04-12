d84080ec9722f66d6b40cdee4c270518c85f05b0
Elena <elena.fedorova.v@gmail.com>
2015-04-12 23:48:55 +0300

    Sudden changes

---
+++ week16/hypnofrog/sketch.js
@@ -0,0 +1,41 @@
+
+
+function preload() {
+    //background img
+   
+}
+
+
+function draw() {
+   
+}
+
+function mouseClicked(event) {
+    // if (this.currentScene) {
+    //     this.currentScene.clicked(mouseX, mouseY);
+    //     event.preventDefault();
+    //     return false;
+    // }
+}
+
+function keyPressed(event) {
+    // if (this.currentScene) {
+    //     this.currentScene.pressed(keyCode);
+    //     event.preventDefault();
+    //     return false;
+    // }
+}
+
+function touchEnded(event) {
+    // if (this.currentScene) {
+    //     this.currentScene.clicked(touchX, touchY);
+    //     event.preventDefault();
+    //     return false;
+    // }
+}
+
+function setup() {
+    
+
+}
+
