cf9596ca4522f9c86bbe14e3b13f6cc2eb191403
Elena <elena.fedorova.v@gmail.com>
2015-04-19 23:33:54 +0300

    week17 moved to week18

---
+++ week18/lordofflies/scene.js
@@ -0,0 +1,354 @@
+//Player
+
+var Player = function(walls) {
+	this.width = width / 16;
+	this.height = height / 8;
+
+	this.start();
+	this.MAX_VELOCITY = 10;
+	this.ACCELERATION = 0.3;
+	this.MAX_ACCELERATION = 1;
+
+	this.construct = walls;
+};
+
+Player.prototype.draw = function() {
+	image(mishaImg, this.position.x, this.position.y, this.width, this.height);
+};
+
+Player.prototype.start = function() {
+	this.velocity = createVector(0, 0);
+	this.acceleration = createVector(0, 0);
+	this.position = createVector(width/2, height/2);
+};
+
+
+Player.prototype.pressed = function(value) {};
+
+Player.prototype.update = function() {
+
+	var mousePosition = createVector(mouseX, mouseY);
+	this.acceleration = mousePosition.sub(this.position); //mx - x, my - y
+	this.acceleration.limit(this.MAX_ACCELERATION); 
+
+	this.velocity.add(this.acceleration).limit(this.MAX_VELOCITY);
+
+	viewPosition.add(this.velocity);
+
+	if (this.collided()) {
+		viewPosition.sub(this.velocity.mult(2));
+		this.velocity = createVector(0, 0);
+	}
+};
+
+Player.prototype.collided = function() {
+	if (
+			this.position.x <= 0 
+		|| this.position.x + this.width >= width 
+		|| this.position.y <= 0 
+		|| this.position.y + this.height >= height) {
+		return true;
+	}
+
+	for (var i in this.construct.walls) {
+		if (this.checkForCollision(this.construct.walls[i])) {
+			return true;
+		}
+	}
+
+	return false;
+};
+
+Player.prototype.checkForCollision = function(mess) {
+	var x1 = mess.position.x;
+	var x2 = mess.position.x + mess.width;
+	var x1p = this.position.x;
+	var x2p = this.position.x + this.width;
+
+	var y1 = mess.position.y;
+	var y2 = mess.position.y + mess.height;
+	var y1p = this.position.y;
+	var y2p = this.position.y + this.height;
+
+	if ((x1 <= x2p && x2 >= x1p) &&
+		(y1 <= y2p && y2 >= y1p)) {
+		return true;
+	} else {
+		return false;
+	}
+};
+
+//Mess
+var Wall = function(x,y) {
+	this.realPosition = createVector(x, y);
+	this.position = createVector(x, y);
+	this.width = 150;
+	this.height = 150;
+};
+
+Wall.prototype.draw = function() {
+	fill(150, 200, 38);
+	rect(this.position.x, this.position.y, this.width, this.height);
+};
+
+Wall.prototype.update = function() {
+	this.position = p5.Vector.sub(this.realPosition, viewPosition);
+};
+
+Wall.prototype.act = function() {
+	// this.y = height+400;
+	// // score++;
+	// drinks+=1;
+	// this.img = drinkImages[Math.floor(random(0, drinkImages.length))];
+};
+
+var Walls = function(){
+	this.walls = [new Wall(0, 0)];
+	for (i = 0; i < 5; i++) {
+   	this.addWall();
+	}
+};
+
+Walls.prototype.addWall = function() {
+	var x = this.walls[this.walls.length-1].position.x + 350;
+	var y = random(25,1250);
+   this.walls.push(new Wall(x,y)); 
+};
+
+
+Walls.prototype.draw = function(){
+	for (var i in this.walls) {
+		this.walls[i].draw();
+	}
+};
+
+Walls.prototype.update = function(){
+	for (var i in this.walls) {
+		this.walls[i].update();
+	}
+};
+
+//Header Text
+
+var HeaderTxt = function(colorTxt, label, y, sizeTxt) {
+	this.x = width / 2;
+	this.y = y;
+	this.sizeTxt = sizeTxt;
+	this.colorTxt = colorTxt;
+	this.label = label;
+};
+
+HeaderTxt.prototype.draw = function() {
+	noStroke();
+
+	fill(this.colorTxt);
+	textSize(this.sizeTxt);
+	textAlign(CENTER);
+	text(this.label, this.x, this.y);
+};
+
+//Distance
+
+
+var ScoreScreen = function() {
+	this.x = width / 20;
+	this.y = height*0.95;
+
+};
+
+ScoreScreen.prototype.start = function(){
+	var countdownTimer = setInterval(this.secondPassed, 1000);
+};
+
+ScoreScreen.prototype.secondPassed = function(){
+	score--;
+};
+
+
+ScoreScreen.prototype.draw = function() {
+	noStroke();
+
+	fill(255, 255, 255);
+	textSize(30);
+	textAlign(LEFT);
+	text("Left: " + score + " sec ", this.x, this.y);
+};
+
+ScoreScreen.prototype.update = function() {
+	if (score <= 0) {
+		gameOver();
+	}
+};
+
+//Drunk score
+
+var DrunkScreen = function() {
+	this.x = width / 20 * 13;
+	this.y = height*0.95;
+};
+
+DrunkScreen.prototype.draw = function(){
+	noStroke();
+	fill(255, 255, 255);
+	textSize(30);
+	textAlign(LEFT);
+	text("Drunk: " + drinks/100 + "%", this.x, this.y);
+};
+
+DrunkScreen.prototype.update = function(){
+	
+};
+
+
+
+
+var FinalScreen = function() {
+	this.x = 6*width / 20;
+	this.y = 12 * height / 20;
+
+};
+
+FinalScreen.prototype.draw = function() {
+	noStroke();
+
+	fill(255, 255, 255);
+	textSize(36);
+	textAlign(LEFT);
+	text("Drunk Score: " + drinks/100 + "%", this.x, this.y);
+};
+
+
+//Buttons
+var Button = function(labelText, action) {
+	this.width = width * 0.33;
+	this.height = 60;
+	this.x = width / 2 - this.width / 2;
+	this.y = height - 50 - this.height / 2;
+
+	this.labelText = labelText;
+	this.action = action;
+};
+
+Button.prototype.draw = function() {
+
+	rectMode(CORNER);
+	fill(0, 0, 0);
+	stroke(255, 255, 255);
+	rect(this.x, this.y, this.width, this.height, 5);
+	fill(255, 255, 255);
+	noStroke();
+	textSize(23);
+	textAlign(CENTER);
+	//noStroke();
+	text(this.labelText, this.x + this.width / 2, this.y + this.height / 2 + 5);
+};
+
+Button.prototype.clicked = function(x, y) {
+	if (this.isMouseInside(x, y)) {
+		this.action();
+	}
+};
+
+Button.prototype.isMouseInside = function(mouseX, mouseY) {
+	return (mouseX >= this.x &&
+		mouseX <= (this.x + this.width) &&
+		mouseY >= this.y &&
+		mouseY <= (this.y + this.height));
+};
+
+
+
+
+//Proto scene
+var Scene = function(background) {
+	this.background = background;
+	this.widgets = [];
+	this.drawBg = true;
+};
+
+Scene.prototype.draw = function() {
+	if (this.drawBg) {
+		background(this.background);
+	}
+	for (var objectId in this.widgets) {
+		this.widgets[objectId].draw();
+	}
+
+};
+
+Scene.prototype.clicked = function(x, y) {
+	console.log("ProtoScene caught click: " + x + " : " + y);
+	for (var objectId in this.widgets) {
+		if (this.widgets[objectId].clicked !== undefined) {
+			this.widgets[objectId].clicked(x, y);
+		}
+	}
+};
+
+
+Scene.prototype.pressed = function(keyCode) {
+	console.log("ProtoScene caught pressed: " + keyCode);
+	for (var objectId in this.widgets) {
+		if (this.widgets[objectId].pressed !== undefined) {
+			this.widgets[objectId].pressed(keyCode);
+		}
+	}
+};
+
+Scene.prototype.start = function() {
+	console.log("ProtoScene started!");
+	for (var objectId in this.widgets) {
+		if (this.widgets[objectId].start !== undefined) {
+			this.widgets[objectId].start();
+		}
+	}
+};
+
+Scene.prototype.update = function() {
+	for (var objectId in this.widgets) {
+		if (this.widgets[objectId].update !== undefined) {
+			this.widgets[objectId].update();
+		}
+	}
+};
+
+var SceneSub = function(x, y) {
+	Scene.call(this, x, y);
+	this.backgroundX = 0;
+	this.drawBg = false;
+};
+
+SceneSub.prototype = Object.create(Scene.prototype);
+
+SceneSub.prototype.update = function() {
+	if (this.backgroundX <= -width) {
+		this.backgroundX = 0;
+	}
+	
+
+	if (drinks < 25 && drinks >= 10 && soundtrack !== soundtrack110) {
+		soundtrack.stop();
+		soundtrack = soundtrack110;
+		soundtrack.loop();
+	} else if (drinks < 40 && drinks >= 25 && soundtrack !== soundtrack120) {
+		soundtrack.stop();
+		soundtrack = soundtrack120;
+		soundtrack.loop();
+	} else if (drinks >= 40 && soundtrack !== soundtrack130) {
+		soundtrack.stop();
+		soundtrack = soundtrack130;
+		soundtrack.loop();
+	} 
+
+	Scene.prototype.update.call(this);
+};
+
+
+SceneSub.prototype.draw = function() {	
+	image(this.background, this.backgroundX + width, 0, width, height);
+	image(this.background, this.backgroundX, 0, width, height);
+	
+	Scene.prototype.draw.call(this);
+
+
+};
---
+++ week18/lordofflies/sketch.js
@@ -0,0 +1,196 @@
+var score = 30;
+var viewPosition;
+var drinks = 0;
+
+function preload() {
+    //background img
+    bg = loadImage('../assets/pavementdark.jpg');
+    bg1 = loadImage('../assets/intro1.jpg');
+    bg2 = loadImage('../assets/rules.jpg');
+    //bg3 = loadImage('../assets/scene3.jpg');
+    bgfinalgood = loadImage('../assets/win.jpg');
+    bgfinalbad = loadImage('../assets/finalbad.jpg');
+
+    //objects img
+    
+    mishaImg = loadImage('../assets/misha1.png');
+    //oldladyImg = loadImage('../assets/oldy.png');
+
+    //sounds
+    soundtrack100 = loadSound('../assets/sound/sound100.wav');
+    soundtrack110 = loadSound('../assets/sound/sound110.wav');
+    soundtrack120 = loadSound('../assets/sound/sound120.wav');
+    soundtrack130 = loadSound('../assets/sound/sound130.wav');
+
+    soundfinal = loadSound('../assets/sound/fanfare.mp3');
+    soundloose = loadSound('../assets/sound/airhorn.mp3');
+    soundclick = loadSound('../assets/sound/drum.mp3');
+
+    drinkImages = [
+        loadImage('../assets/cocktail1.png'),
+        loadImage('../assets/cocktail2.png'),
+        loadImage('../assets/cocktail3.png'),
+        loadImage('../assets/cocktail4.png'),
+        loadImage('../assets/cocktail5.png')
+    ];
+}
+
+
+function draw() {
+    //this.scene1.draw();
+    //this.scene2.draw();
+    this.currentScene.draw();
+    this.currentScene.update();
+    //this.scenefinalbad.draw();
+    //this.scenefinalgood.draw();
+
+	if (keyIsDown(UP_ARROW)) {
+		viewPosition.y -= 1;
+	} else if (keyIsDown(DOWN_ARROW)) {
+		viewPosition.y += 1;
+	}
+
+
+}
+
+function mouseClicked(event) {
+    if (this.currentScene) {
+        this.currentScene.clicked(mouseX, mouseY);
+        event.preventDefault();
+        return false;
+    }
+}
+
+function keyPressed(event) {
+    if (this.currentScene) {
+        this.currentScene.pressed(keyCode);
+        event.preventDefault();
+        return false;
+    }
+}
+
+function touchEnded(event) {
+    if (this.currentScene) {
+        this.currentScene.clicked(touchX, touchY);
+        event.preventDefault();
+        return false;
+    }
+}
+
+function setup() {
+    var screenSize;
+    viewPosition = createVector(0,0);
+
+    if (window.innerWidth > window.innerHeight) {
+        screenSize = Math.min(200, window.innerHeight / 3);
+        createCanvas(screenSize * 4, screenSize * 3);
+    } else {
+        screenSize = Math.min(200, window.innerWidth / 4);
+        createCanvas(screenSize * 4, screenSize * 3);
+    }
+
+
+    //noLoop();
+    //scenes
+
+    this.currentScene = this.introScene = createIntroScene(this);
+    this.ruleScene = createRuleScene(this);
+    this.gameScene = createGameScene(this);
+    this.finalScene = createFinalScene(this);
+    this.looseScene = createLooseScene(this);
+    
+}
+
+function gameOver() {
+    this.currentScene = this.finalScene;
+    this.soundtrack.stop();
+    this.soundfinal.play();
+}
+
+
+function loose() {
+    this.currentScene = this.looseScene;
+    this.soundtrack.stop();
+    this.soundloose.play();
+}
+
+function createGameScene(game) {
+    var scene = new SceneSub(bg);
+
+    var walls = new Walls();
+    scene.widgets.push(walls);
+
+    game.misha = new Player(walls);
+    scene.widgets.push(misha);
+    
+    
+    scene.widgets.push(new ScoreScreen());
+    scene.widgets.push(new DrunkScreen());
+
+
+    return scene;
+}
+
+var restartFunction = function(game) {
+    return function() {
+        clear();
+        game.currentScene = game.gameScene;
+        game.gameScene.start();
+        score = 20000;
+        drinks = 0;
+        soundfinal.stop();
+        soundloose.stop();
+
+        game.soundtrack = soundtrack100;
+        game.soundtrack.loop();  
+    };
+};
+function createFinalScene(game) {
+    var scene = new Scene(bgfinalgood);
+    var btnfinal = new Button("Restart", restartFunction(game));
+
+    var finalScore = new FinalScreen();
+
+    scene.widgets.push(finalScore);
+    scene.widgets.push(btnfinal);
+
+    return scene;
+}
+
+function createLooseScene(game) {
+    var scene = new Scene(bgfinalbad);
+    var btnfinal = new Button("Restart", restartFunction(game));
+
+    var finalScore = new FinalScreen();
+
+    scene.widgets.push(finalScore);
+    scene.widgets.push(btnfinal);
+
+    return scene;
+}
+
+function createRuleScene(game) {
+
+    var scene = new Scene(bg2);
+
+    var btn2 = new Button("Go,go,go!", function() {
+        clear();
+        game.currentScene = game.gameScene;  
+        game.soundtrack = soundtrack100;          
+        game.soundtrack.loop();
+
+        game.gameScene.start();
+    });
+    scene.widgets.push(btn2);
+    return scene;
+}
+
+function createIntroScene(game) {
+    var scene = new Scene(bg1);
+
+    var btn1 = new Button("Start Me", function() {
+        game.currentScene = game.ruleScene;
+    });
+    scene.widgets.push(btn1);
+    return scene;
+}
