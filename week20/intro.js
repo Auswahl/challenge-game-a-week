90d76a6cebbfa98b7074a18ae49e2e9f6b5b1a25
Elena <elena.fedorova.v@gmail.com>
2015-05-30 18:48:15 +0300

    Mobile template

--- week20/intro.js
+++ week20/intro.js
@@ -1,80 +1,58 @@
-var intro = function(game) {
-
-};
-intro.content = [
-	"",
-	"Well, hello there!",
-	"Welcome to Roscosmos, newbie.",
-	'You are going to lead the\n"Progress" cargo ship to the \nInternational Space Station.',
-	" ",
-	"Why you?",
-	'Why not!\nas we say at Roscosmos',
-	"Just hold spaceship, pull\nand keep throwing higher\nand higher",
-	"Good luck!"
-
-
-];
-
-
-intro.prototype.init = function() {};
-intro.prototype.preload = function() {
-
-	this.index = 0;
-	this.line = '';
-
-	var bg = game.add.tileSprite(0, 0, 800, 600, 'introBg');
-
-	this.text = game.add.text(32, 380, '', {
-		font: "30pt Courier",
-		fill: "#00ff00",
-		stroke: "#119f4e",
-		strokeThickness: 2
-	});
-
-	this.nextLine();
-
-	this.game.load.image('alien', 'assets/ISS.png');
-	this.game.load.image('cow', 'assets/regress.png');
-
-	this.game.load.physics('physicsData', 'assets/polygon_data.json');
-	this.game.load.tilemap('map', 'assets/space.json', null, Phaser.Tilemap.TILED_JSON);
-	this.game.load.image('space_tilesets', 'assets/space_tilesets.png');
-
-	this.game.load.audio('hit', 'assets/hit.mp3');
-	this.game.load.audio('final', 'assets/final.wav');
-	this.game.load.audio('soundtrack', 'assets/Sunrise.mp3');
-
-	this.game.load.image('bg1', 'assets/back1.jpg');
-	this.game.load.image('winBg', 'assets/finalwin.jpg');
-	this.game.load.image('looseBg', 'assets/finalloose.jpg');
-
-
-};
-intro.prototype.create = function() {
+var TheGame = function(game) {
 
 };
 
-intro.prototype.updateLine = function() {
+TheGame.prototype = {
+	init: function() {
+		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+		this.scale.fullScreenScaleMode = Phaser.ScaleManager.SHOW_ALL;
+		game.scale.maxWidth = 800;
+		game.scale.maxHeight = 600;
+		game.scale.pageAlignVertically = true;
+		game.scale.pageAlignHorizontally = true;
 
-	if (this.line.length < intro.content[this.index].length) {
-		this.line = intro.content[this.index].substr(0, this.line.length + 1);
-		this.text.setText(this.line);
-	} else {
-		//  Wait 2 seconds then start a new line
-		game.time.events.add(Phaser.Timer.SECOND * 1, this.nextLine, this);
+		if (!this.game.device.desktop) {
+			this.scale.forceOrientation(true, false);
+			this.scale.enterIncorrectOrientation.add(this.enterIncorrectOrientation, this);
+			this.scale.leaveIncorrectOrientation.add(this.leaveIncorrectOrientation, this);
+
+
+		}
+		game.scale.refresh();
+	},
+
+	preload: function() {
+
+		game.load.image("bg", "assets/background.jpg");
+		game.load.image("buttonStart", "assets/btneng.jpg");
+
+	},
+
+	create: function() {
+		var bg = game.add.sprite(0, 0, 'bg');
+		bg.height = 600;
+		bg.width = 800;
+
+		button = this.game.add.button(
+			game.world.centerX,
+			game.world.centerY + game.world.height / 3,
+			'buttonStart', this.setFullScreen, this);
+		button.anchor.setTo(0.5, 0.5);
+	},
+
+	setFullScreen: function() {
+		if (!this.game.device.desktop) {
+			game.scale.startFullScreen();
+		}
+	},
+
+	enterIncorrectOrientation: function() {
+		this.orientated = false;
+		document.getElementById('orientation').style.display = 'block';
+	},
+
+	leaveIncorrectOrientation: function() {
+		this.orientated = true;
+		document.getElementById('orientation').style.display = 'none';
 	}
-
-};
-
-intro.prototype.nextLine = function() {
-
-	this.index++;
-
-	if (this.index < intro.content.length) {
-		this.line = '';
-		game.time.events.repeat(40, intro.content[this.index].length + 1, this.updateLine, this);
-	} else {
-		this.game.state.start("TheGame");
-	}
-
 };
--- week23/script.js
+++ week23/script.js
@@ -2,9 +2,9 @@ var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example');
 
 // game.state.add("Cover", cover);
 // game.state.add("Intro", intro);
-game.state.add("Game", TheGame);
+game.state.add("TheGame", TheGame);
 // game.state.add("GameOver", gameOver);
 // game.state.add("Win", win);
 // game.state.add("Loose", loose);
 
-game.state.start("Game");
+game.state.start("TheGame");
